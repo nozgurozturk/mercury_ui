c8511986de76ced921b2602b370a099b64a94452
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-06-17 01:15:36 +0300

    Collect all interfaces in same file

---
+++ src/interfaces/index.ts
@@ -0,0 +1,61 @@
+import * as React from 'react'
+
+/**
+ * @interface IDiv
+ */
+export interface IDiv extends React.DetailedHTMLProps
+  <React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> { }
+
+/**
+ * @interface IButton
+ */
+export interface IButton extends React.DetailedHTMLProps
+  <React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> { }
+
+/**
+ * @interface IElement
+ */
+export interface IElement extends React.DetailedHTMLProps
+  <React.HTMLAttributes<HTMLElement>, HTMLElement> { }
+
+/**
+ * @interface ILink
+ */
+export interface ILink extends React.DetailedHTMLProps
+  <React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> { }
+
+/**
+ * @interface IInput
+ */
+export interface IInput extends React.DetailedHTMLProps
+  <React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> { }
+
+/**
+ * @interface ILiElement
+ */
+export interface ILiElement extends React.DetailedHTMLProps
+  <React.LiHTMLAttributes<HTMLLIElement>, HTMLLIElement> { }
+
+/**
+ * @interface IUlElement
+ */
+export interface IUlElement extends React.DetailedHTMLProps
+  <React.HTMLAttributes<HTMLUListElement>, HTMLUListElement> { }
+
+/**
+ * @interface IParagraph
+ */
+export interface IParagraph extends React.DetailedHTMLProps
+  <React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> { }
+
+/**
+ * @interface IHeading
+ */
+export interface IHeading extends React.DetailedHTMLProps
+  <React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement> { }
+
+/**
+ * @interface ITable
+ */
+export interface ITable extends React.DetailedHTMLProps
+  <React.TableHTMLAttributes<HTMLTableElement>, HTMLTableElement> { }
