04e83b4efcd721442ac2919ed3ce1b2e53456b24
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-06-17 01:13:42 +0300

    Refactor Radio component, style and add test

--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -1,41 +1,60 @@
 import * as React from 'react'
-import classNames from 'classnames'
-// Style
-import '../../styles/components/_radio.scss'
+import cx from 'classnames'
+// Interface
+import { IInput } from '../../interfaces'
 
-interface IRadio extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> { }
-
-// TODO: Add ForwardRef
-
-interface RadioProps extends IRadio {
+interface RadioProps extends IInput {
+  /**
+   * Inline Text of Checkbox
+   */
   label?: React.ReactNode,
+  /**
+   * Error Status of Checkbox
+   */
   error?: boolean,
-  message?: React.ReactNode
-  success?: boolean,
-  id?: string
-  name?: string,
+  /**
+   * When error is true, shows this message
+   */
+  errorMessage?: React.ReactNode
+  /**
+   * External Ref object
+   */
+  inputRef?: React.MutableRefObject<HTMLInputElement>
+  /**
+   * Additional classes
+   */
   className?: string,
 }
 
 export class Radio extends React.PureComponent<RadioProps> {
 
   public render() {
-    const { id, name, label, error, message, success, className, ...props } = this.props;
-    const radioClasses = classNames(
-      'mrc-radio',
-      success && 'mrc-success',
-      error && 'mrc-error',
+    const {
+      id,
+      label,
+      error,
+      errorMessage,
+      disabled,
+      className,
+      inputRef,
+      ...props
+    } = this.props
+
+    const radioClasses = cx(
+      'm-radio',
+      error && 'm-error',
+      disabled && 'm-input--disabled',
       className
     )
     return (
       <div className={radioClasses}>
-        <input id={id} name={name} type="radio" {...props} />
+        <input id={id} name={name} type="radio" disabled={disabled} {...props} />
         <label htmlFor={id} />
         <span>{label}</span>
         {
-          message &&
-          <div className="mrc-field-message">
-            {message}
+          error && errorMessage &&
+          <div className="m-field__message">
+            {errorMessage}
           </div>
         }
       </div>
--- src/components/Radio/__test__/Radio.spec.tsx
+++ src/components/Radio/__test__/Radio.spec.tsx
@@ -1,21 +1,43 @@
+/* eslint-disable no-undef */
+import * as React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import { Radio } from '../Radio'
+import * as faker from 'faker'
 
-import React from "react";
-import { render } from "@testing-library/react";
-import Radio from "./Radio";
-import { RadioProps } from "./Radio.types";
-describe("Test Component", () => {
-  let props: RadioProps;
+let text: string
+describe('Text', () => {
+  afterEach(cleanup)
   beforeEach(() => {
-    props = {
-      foo: "bar"
-    };
-  });
-  const renderComponent = () => render(<Radio {...props} />);
-  it("should render foo text correctly", () => {
-    props.foo = "harvey was here";
-    const { getByTestId } = renderComponent();
-    const component = getByTestId("Radio");
-    expect(component).toHaveTextContent("harvey was here");
-  });
-});
+    text = faker.random.word()
+  })
 
+  test('should render radio', () => {
+    const { container } = render(<Radio id="cx" />)
+    expect(container.querySelector('input[type="radio"]')).toBeInTheDocument()
+  })
+
+  test('should render radio text', () => {
+    const { getByText } = render(<Radio id="cx" label={text} />)
+    expect(getByText(text)).toHaveTextContent(text)
+  })
+
+  test('should render error message', () => {
+    const { getByText } = render(<Radio id="cx" label={text} error={true} errorMessage="something wrong" />)
+    expect(getByText(/something wrong/i)).toBeInTheDocument()
+  })
+
+  test('should checked changes when radio is clicked', () => {
+    const fn = jest.fn()
+    const { container } = render(<Radio id="cx" label={text} onClick={fn} />)
+    fireEvent.click(container.querySelector('label'))
+    expect((container.querySelector('input[type="radio"]') as HTMLInputElement).checked).toEqual(true)
+  })
+
+  test('should checked not changes when disabled radio is clicked', () => {
+    const fn = jest.fn()
+    const { container } = render(<Radio disabled id="cx" label={text} onClick={fn} />)
+    fireEvent.click(container.querySelector('label'))
+    expect((container.querySelector('input[type="radio"]') as HTMLInputElement).checked).toEqual(false)
+  })
+
+})
