66aafd2d04f76085196901a1940440629b556c38
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-06-17 01:17:41 +0300

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,40 +2,56 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import jsx from 'acorn-jsx';
 import typescript from '@rollup/plugin-typescript';
 // import typescript from "rollup-plugin-typescript2";
-// import { terser } from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser'
 import sass from "rollup-plugin-sass";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import analyze from 'rollup-plugin-analyzer'
-import packageJson from './package.json'
+// import packageJson from './package.json'
 export default {
   input: [
   "src/index.ts",
+  "src/components/Button/Button.tsx",
+  "src/components/Checkbox/Checkbox.tsx",
+  "src/components/Collapse/Collapse.tsx",
+  "src/components/Collapse/Panel.tsx",
+  "src/components/Drawer/Drawer.tsx",
+  "src/components/Grid/Row.tsx",
+  "src/components/Grid/Col.tsx",
+  "src/components/Icon/Icon.tsx",
+  "src/components/Loader/Loader.tsx",
+  "src/components/Menu/Menu.tsx",
+  "src/components/Modal/Modal.tsx",
+  "src/components/NumberInput/NumberInput.tsx",
+  "src/components/Radio/Radio.tsx",
+  "src/components/Result/Result.tsx",
+  // "src/components/Select/Select.jsx",
+  "src/components/Skeleton/Skeleton.tsx",
+  "src/components/Tab/Tab.tsx",
+  "src/components/Tab/TabGroup.tsx",
+  "src/components/Table/Table.tsx",
+  "src/components/Text/Text.tsx",
+  "src/components/TextInput/TextInput.tsx",
+  "src/components/Title/Title.tsx",
   ],
   output: [
     {
-      file: packageJson.main,
+      dir: "build",
       format: "cjs",
-      sourcemap: true
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
       sourcemap: true
     }
   ],
-  acornInjectPlugins: [jsx()],
+  preserveModules: true,
+  // acornInjectPlugins: [jsx()],
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({   
-    jsx: 'preserve', 
-  }),
+    typescript(),
     sass({
-      insert: true
+      insert: false
     }),
-    // terser(),
+    terser(),
     analyze()
   ]
 };
