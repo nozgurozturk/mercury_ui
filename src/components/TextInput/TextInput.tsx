100925e3d3b54c674afd61dfec748f5acf5f25ac
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-06-17 01:14:54 +0300

    Refactor TextInput component, style and add test

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,34 +1,67 @@
 
 import * as React from 'react'
-import classNames from 'classnames'
+import cx from 'classnames'
 // Types
 import { size, status } from './TextInput.types'
-// Style
-import '../../styles/components/_textinput.scss'
+// Interface
+import { IInput } from '../../interfaces'
 // Components
 import { Icon } from '../Icon'
 import { Loader } from '../Loader'
 
-interface ITextInput extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> { }
+// interface ITextInput extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> { }
 
-interface TextInputProps extends ITextInput {
+interface TextInputProps extends IInput {
+  /**
+   * Size of Input
+   * @default 'default'
+   */
   inputSize?: size,
+  /**
+   * Explanation text of Input below of the label
+   */
   helperText?: React.ReactNode,
+  /**
+   * Label of the Input
+   */
   label?: React.ReactNode,
+  /**
+   * When input is loading shows loading indicator and disabled
+   */
   loading?: boolean,
+  /**
+   * Status of Input 'warning' | 'success' | 'error'
+   */
   status?: status,
+  /**
+   * Message when status is equal to error
+   */
   errorMessage?: React.ReactNode,
+  /**
+   * Prefix Icon of Input
+   */
   prefix?: string,
+  /**
+   * Suffix Icon of Input
+   */
   suffix?: string,
+  /**
+   * External react ref object
+   */
   inputRef?: React.MutableRefObject<HTMLInputElement>
+  /**
+   * Additional Classes
+   */
   className?: string,
 }
 
 export class TextInput extends React.PureComponent<TextInputProps> {
-
+  public static defaultProps = {
+    inputSize: 'default',
+  }
   public render() {
     const {
-      inputSize = "default",
+      inputSize,
       helperText,
       label,
       status,
@@ -42,39 +75,30 @@ export class TextInput extends React.PureComponent<TextInputProps> {
       ...props
     } = this.props
 
-    const inputClasses = classNames(
-      'mrc-text-input',
-      (disabled || loading) && 'mrc-input-disabled',
-      status && `mrc-${status}`,
-      className,
+    const inputClasses = cx(
+      'm-input__text',
+      (disabled || loading) && 'm-input--disabled',
+      status && `m-${status}`,
+      className
     )
-    const wrapperClasses = classNames(
-      'mrc-text-input__wrapper',
-      inputSize && `mrc-input-${inputSize}`,
+    const wrapperClasses = cx(
+      'm-input__text__wrapper',
+      inputSize && `m-input--${inputSize}`
     )
 
     return (
       <div className={inputClasses}>
-        {label && <label className="mrc-text-input__label">{label}</label>}
-        {helperText && <div className="mrc-text-input__helper">{helperText}</div>}
+        {label && <label className="m-input__label">{label}</label>}
+        {helperText && <div className="m-input__helper">{helperText}</div>}
         <div className={wrapperClasses}>
-          {prefix && <Icon name={prefix} size={24} />}
+          {prefix && <Icon className="m-icon--prefix" name={prefix} size={16} />}
           <input ref={inputRef} disabled={disabled || loading} type="text" {...props} />
-          {suffix && <Icon name={suffix} size={24} />}
-          {status && !loading && <Icon name={status} size={24} />}
+          {status && !loading && (<Icon className="m-icon--status" name={`${status}--filled`} intent={status} size={16} />)}
           {loading && <Loader active={loading} size={12} />}
+          {suffix && <Icon className="m-icon--suffix" name={suffix} size={16} />}
         </div>
-        {
-          status === 'error' && errorMessage &&
-          <div className="mrc-field-message">
-            {errorMessage}
-          </div>
-        }
+        {(status === 'error' && errorMessage) && <div className="m-field__message">{errorMessage}</div>}
       </div>
     )
   }
 }
-
-// export const TextInput = React.forwardRef((props: TextInputProps, ref: React.MutableRefObject<any>) => (
-//   <BaseTextInput  {...props} inputRef={ref} />
-// ))
--- src/components/TextInput/__test__/TextInput.spec.tsx
+++ src/components/TextInput/__test__/TextInput.spec.tsx
@@ -1,21 +1,46 @@
+/* eslint-disable no-undef */
+import * as React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import { TextInput } from '../TextInput'
+import * as faker from 'faker'
 
-import React from "react";
-import { render } from "@testing-library/react";
-import TextInput from "./TextInput";
-import { TextInputProps } from "./TextInput.types";
-describe("Test Component", () => {
-  let props: TextInputProps;
+let text: string
+describe('Text', () => {
+  afterEach(cleanup)
   beforeEach(() => {
-    props = {
-      foo: "bar"
-    };
-  });
-  const renderComponent = () => render(<TextInput {...props} />);
-  it("should render foo text correctly", () => {
-    props.foo = "harvey was here";
-    const { getByTestId } = renderComponent();
-    const component = getByTestId("TextInput");
-    expect(component).toHaveTextContent("harvey was here");
-  });
-});
+    text = faker.random.word()
+  })
 
+  test('should render text input', () => {
+    const { container } = render(<TextInput id="cx" />)
+    expect(container.querySelector('input[type="text"]')).toBeInTheDocument()
+  })
+
+
+  test('should render label', () => {
+    const { queryByText } = render(<TextInput label={text} id="cx" />)
+    expect(queryByText(text)).toBeInTheDocument()
+  })
+
+  test('should render helper', () => {
+    const { queryByText } = render(<TextInput helperText={text} id="cx" />)
+    expect(queryByText(text)).toBeInTheDocument()
+  })
+
+  test('should render errorMessage when status is equal to error', () => {
+    const { queryByText } = render(<TextInput status="error" errorMessage={text} id="cx" />)
+    expect(queryByText(text)).toBeInTheDocument()
+  })
+
+  test('should not render errorMessage when status is not equal to error', () => {
+    const { queryByText } = render(<TextInput errorMessage={text} id="cx" />)
+    expect(queryByText(text)).not.toBeInTheDocument()
+  })
+
+  test('should invoke callback function when change input value', () => {
+    const fn = jest.fn()
+    const { queryByTestId } = render(<TextInput onChange={fn} id="cx" data-testid="cx" />)
+    fireEvent.change(queryByTestId('cx'), { target: { value: text } })
+    expect(fn).toBeCalledTimes(1)
+  })
+})
