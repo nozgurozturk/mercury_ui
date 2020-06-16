4ace73b24983e421c69bd34e282aaa008bf1aa19
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-06-17 01:11:50 +0300

    Refactor Checkbox component, style and add test

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,35 +1,61 @@
 import * as React from 'react'
-import classNames from 'classnames'
-// Style
-import '../../styles/components/_checkbox.scss'
-interface ICheckbox extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> { }
+import cx from 'classnames'
+// Interface
+import { IInput } from '../../interfaces'
 
 // TODO: Add ForwardRef
-interface CheckboxProps extends ICheckbox {
+interface CheckboxProps extends IInput {
+  /**
+   * Inline Text of Checkbox
+   */
   label?: React.ReactNode,
+  /**
+   * Error Status of Checkbox
+   */
   error?: boolean,
+  /**
+   * When error is true, shows this message
+   */
   errorMessage?: React.ReactNode
-  id?: string
+  /**
+   * External Ref object
+   */
+  inputRef?: React.MutableRefObject<HTMLInputElement>
+  /**
+   * Additional classes
+   */
   className?: string,
 }
 
 export class Checkbox extends React.PureComponent<CheckboxProps> {
 
   public render() {
-    const { id, label, error, errorMessage, className, ...props } = this.props;
-    const checkBoxClasses = classNames(
-      'mrc-checkbox',
-      error && 'mrc-error',
+    const {
+      id,
+      label,
+      error,
+      disabled,
+      errorMessage,
+      className,
+      inputRef,
+      ...props
+    } = this.props
+
+    const checkBoxClasses = cx(
+      'm-checkbox',
+      error && 'm-error',
+      disabled && 'm-input--disabled',
       className
     )
+
     return (
       <div className={checkBoxClasses}>
-        <input id={id} type="checkbox" {...props} />
+        <input ref={inputRef} id={id} type="checkbox" disabled={disabled} {...props} />
         <label htmlFor={id} />
         <span>{label}</span>
         {
           error && errorMessage &&
-          <div className="mrc-field-message">
+          <div className="m-field__message">
             {errorMessage}
           </div>
         }
--- src/components/Checkbox/__test__/Checkbox.spec.tsx
+++ src/components/Checkbox/__test__/Checkbox.spec.tsx
@@ -1,21 +1,42 @@
+/* eslint-disable no-undef */
+import * as React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import { Checkbox } from '../Checkbox'
+import * as faker from 'faker'
 
-import React from "react";
-import { render } from "@testing-library/react";
-import Checkbox from "./Checkbox";
-import { CheckboxProps } from "./Checkbox.types";
-describe("Test Component", () => {
-  let props: CheckboxProps;
+let text: string
+describe('Text', () => {
+  afterEach(cleanup)
   beforeEach(() => {
-    props = {
-      foo: "bar"
-    };
-  });
-  const renderComponent = () => render(<Checkbox {...props} />);
-  it("should render foo text correctly", () => {
-    props.foo = "harvey was here";
-    const { getByTestId } = renderComponent();
-    const component = getByTestId("Checkbox");
-    expect(component).toHaveTextContent("harvey was here");
-  });
-});
+    text = faker.random.word()
+  })
 
+  test('should render checkbox', () => {
+    const { container } = render(<Checkbox id="cx" />)
+    expect(container.querySelector('input[type="checkbox"]')).toBeInTheDocument()
+  })
+
+  test('should render checkbox text', () => {
+    const { getByText } = render(<Checkbox id="cx" label={text} />)
+    expect(getByText(text)).toHaveTextContent(text)
+  })
+
+  test('should render error message', () => {
+    const { getByText } = render(<Checkbox id="cx" label={text} error={true} errorMessage="something wrong" />)
+    expect(getByText(/something wrong/i)).toBeInTheDocument()
+  })
+
+  test('should checked changes when checkbox is clicked', () => {
+    const fn = jest.fn()
+    const { container } = render(<Checkbox id="cx" label={text} onClick={fn} />)
+    fireEvent.click(container.querySelector('label'))
+    expect((container.querySelector('input[type="checkbox"]') as HTMLInputElement).checked).toEqual(true)
+  })
+
+  test('should checked not changes when disabled checkbox is clicked', () => {
+    const fn = jest.fn()
+    const { container } = render(<Checkbox disabled id="cx" label={text} onClick={fn} />)
+    fireEvent.click(container.querySelector('label'))
+    expect((container.querySelector('input[type="checkbox"]') as HTMLInputElement).checked).toEqual(false)
+  })
+})
